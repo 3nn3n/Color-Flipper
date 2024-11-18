41bd1309da7334ea41c9f2b330476bdb708fa9e3
3nn3n <nitesh_negi@outlook.com>
2024-11-18 21:18:18 +0530

    Update color-flipper.js

--- color-flipper.js
+++ color-flipper.js
@@ -4,10 +4,10 @@ const colorCode = document.getElementById("color-code");
 function getRandomHexColor() {
   const hex = Math.floor(Math.random() * 16777215).toString(16);
   return `#${hex.padStart(6, "0")}`;
+}
 
 button.addEventListener("click", () => {
   const newColor = getRandomHexColor();
   document.body.style.backgroundColor = newColor;
   colorCode.textContent = newColor;
 });
-}
